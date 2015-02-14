2a511de9b3430aecbfd53eedc444230b7e3cb124
Darmody <eterlf41@gmail.com>
2015-02-14 22:22:02 +0800

    修改路由

--- index.js
+++ index.js
@@ -13,15 +13,16 @@ if(process.env.NODE_ENV !== 'test'){
   app.use(logger());
 }
 
-app.use(cors());
+//app.use(cors());
 app.use(koaBody({
     multipart:true,
     formidable: {
         uploadDir: __dirname + '/tmp'
     }
 }));
-app.use(router(app));
-app.use(mount('/aria2', require('./controllers/aria2.js').middleware()));
+//app.use(router(app));
+//app.use(mount('/aria2', require('./controllers/aria2.js').middleware()));
+app.use(require('./controllers/aria2.js').middleware());
 
 //module.exports = app;
 exports.server = app;
